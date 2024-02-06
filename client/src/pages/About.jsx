dcb4097e8b4a3886d7e79e218448b1bc35eb8089
Siddharth Chaudhary <siddharthch22@gmail.com>
2024-02-06 23:17:29 +0530

    Completed the website

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 export default function About() {
     return (
-        <div>
-            About
+        <div className='py-20 px-4 mx-w-6xl mx-auto'>
+            <h1 className='text-3xl font-bol mb-4 text-slate-800'>About Estate solutions </h1>
+            <p className='mb-4 text-slate-700 '>Estate solutions is a project created by Siddharth Chaudhary a prefinal year student pursuing Integrated BTech-MTech Dual Degree from LNMIIT Jaipur. This project is created for learning purpose and is deployed for visualisation. Details present on this website are totally random and does not correspond to any real property. </p>
+            <p className='mb-4 text-slate-700 '>This project is built uses MongoDB as database, Express for creating a http server, React with redux toolkit to manage the state is used in frontend and NodeJS is used for server thus using the famous <span className='font-semibold'>MERN stack</span> also the Swiper library is used for Swipe Navigation.It is deployed on Render and project code can be found on my github. </p>
+
         </div>
     )
 }
