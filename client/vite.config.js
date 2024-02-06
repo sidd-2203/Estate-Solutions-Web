a726c1b6abf56c661a0f3975b6d9eefb57b8bc41
Siddharth Chaudhary <siddharthch22@gmail.com>
2024-02-06 23:45:20 +0530

    configured for deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,8 +6,9 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
-        secure: false,
+        target: 'https://esatate-solutions.onrender.com/',
+        secure: true,
+        changeOrigin: true,
       }
     }
   },
